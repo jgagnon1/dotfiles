6af4a121fb4bcdc2f43cccba5a87be1c0ee9775d
jerome-paytm <jerome.gagnon@paytm.com>
2021-10-06 09:52:01 -0400

    Dotfile updates with alias/abbr for kubectl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 # Set PATH
-set -gx PATH $HOME/.cargo/bin /usr/local/go/bin $PATH
+set -gx PATH $HOME/.cargo/bin /usr/local/go/bin /usr/local/opt/gnu-tar/libexec/gnubin $PATH
 
 # Aliases
 alias vim='nvim'
@@ -12,49 +12,12 @@ set -g theme_display_docker_machine yes
 set -g theme_color_scheme gruvbox
 
 # Terraform alias
-alias t terraform
-alias ts tfswitch
+abbr t terraform
+abbr ts tfswitch
+abbr tsi 'tfswitch && terraform init'
 
-# Kubectl alias
-alias k kubectl
-alias kd 'k describe'
-alias kg 'k get'
-alias kaf 'k apply -f'
-alias kdel 'k delete'
-alias ke 'k edit'
-alias kccc 'k config current-context'
-alias kcdc 'k config delete-context'
-alias kcsc 'k config set-context'
-alias kcuc 'k config use-context'
-alias kdd 'kd deployment'
-alias kdeld 'kdel deployment'
-alias kdeli 'kdel ingress'
-alias kdelp 'kdel pods'
-alias kdels 'kdel svc'
-alias kdelsec 'kdel secret'
-alias kdi 'kd ingress'
-alias kdp 'kd pods'
-alias kds 'kd svc'
-alias kdsec 'kd secret'
-alias ked 'ke deployment'
-alias kei 'ke ingress'
-alias kep 'ke pods'
-alias kes 'ke svc'
-alias keti 'k exec -ti'
-alias kgd 'kg deployment'
-alias kgi 'kg ingress'
-alias kgp 'kg pods'
-alias kgrs 'kg rs'
-alias kgs 'kg svc'
-alias kgsec 'kg secret'
-alias kl 'k logs'
-alias klf 'k logs -f'
-alias krh 'k rollout history'
-alias krsd 'k rollout status deployment'
-alias kru 'k rollout undo'
-alias ksd 'k scale deployment'
-
-source /usr/local/opt/asdf/asdf.fish
+test -f /usr/local/opt/asdf/asdf.fish && source /usr/local/opt/asdf/asdf.fish
+test -f ~/.kubectl_aliases.fish && source ~/.kubectl_aliases.fish
 
 # Fixes macOS slow tab completion
 # See https://github.com/fish-shell/fish-shell/issues/6270
