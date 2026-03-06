e5c8c63324a64a32d61dafaa5910fbe206e636ff
jgagnon <jgagnon@wealthsimple.com>
2026-03-06 12:41:48 -0500

    darwin: work machine homebrew packages and postgresql

--- darwin/hosts/jgagnon-wsmb.nix
+++ darwin/hosts/jgagnon-wsmb.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ../configuration.nix ];
@@ -7,4 +7,25 @@
 
   # Leave /etc/zshrc, /etc/zshenv, /etc/zprofile to the company MDM tool
   programs.zsh.enable = false;
+
+  # Work-specific Homebrew packages
+  homebrew = {
+    taps = [ "wealthsimple/tap" ];
+    brews = [
+      "gh"
+      "gitleaks"
+      "jq"
+      "lefthook"
+      "mise"
+      "watchman"
+      "wealthsimple/tap/wolfman"
+      "yq"
+    ];
+  };
+
+  # Local PostgreSQL (only accessible on this machine)
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
+  };
 }
